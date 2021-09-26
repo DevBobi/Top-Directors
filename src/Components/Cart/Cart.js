85913924368dcb8b4f632fae6131712afa70df11
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2021-09-26 18:20:57 +0600

    cart style updated

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Cart.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
 const Cart = (props) => {
     console.log(props);
     const userIcon = <FontAwesomeIcon icon={faUser} />
+    const dollarIcon = <FontAwesomeIcon icon={faDollarSign} />
     const { user } = props;
     const { userName } = props;
 
@@ -18,7 +19,7 @@ const Cart = (props) => {
     return (
         <div className="cart">
             <h1>{userIcon} User: {props.user.length}</h1>
-            <h5>Total: {total} $</h5>
+            <h5>Total: {total} {dollarIcon}</h5>
             {
                 userName.map(name => <li key={name}> {name}</li>)
             }
